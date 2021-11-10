09215cf62f9cff9ffe8076a930ef8f2f3bc86b5e
Calvin <cd-public@users.noreply.github.com>
2021-11-10 14:26:32 -0800

    Update hash.py

--- short/hash.py
+++ short/hash.py
@@ -1,25 +1,25 @@
 # hash.py
+from operator import gt,lt,mul,floordiv
 
 class HashTable:
 
     def __init__(self):
         self.t = [set() for n in range(4)]
         
-    def insert(self,arg):
-        self.t[hash(arg)%len(self.t)].add(arg)
-        if sum([len(l) for l in self.t]) > len(self.t) // 2:
+    def __rehash(self, f1, f2):
+        if f1(sum([len(l) for l in self.t]), len(self.t) // 2):
             f = [n for l in self.t for n in l]
-            self.t = [set() for n in range(len(self.t) * 2)]
+            self.t = [set() for n in range(f2(len(self.t),2))]
             for n in f:
                 self.insert(n)
+        
+    def insert(self,arg):
+        self.t[hash(arg)%len(self.t)].add(arg)
+        self.__rehash(gt,mul)
                 
     def remove(self,arg):
         self.t[hash(arg)%len(self.t)].discard(arg)
-        if sum([len(l) for l in self.t]) < len(self.t) // 2:
-            f = [n for l in self.t for n in l]
-            self.t = [set() for n in range(len(self.t) // 2)]
-            for n in f:
-                self.insert(n)
+        self.__rehash(lt,floordiv)
                 
     def contains(self,arg):
         return arg in self.t[hash(arg)%len(self.t)]

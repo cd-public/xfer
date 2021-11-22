b5e12249c5a967448c7fc92b805ef05525d108e5
Calvin <cd-public@users.noreply.github.com>
2021-11-21 23:14:52 -0800

    Update rsa.c

--- inc/rsa.c
+++ inc/rsa.c
@@ -57,6 +57,7 @@ uint64_t getPrime(){
     return i;
 }
 
+// memory leak here
 uint64_t *getPrimes(uint64_t seed){
 	uint64_t *ret = (uint64_t *)malloc(2 * sizeof(uint64_t));
 	srand(seed);
@@ -65,13 +66,15 @@ uint64_t *getPrimes(uint64_t seed){
 }
 
 // write tests along the way, this could be main
+// frees memory
 uint64_t getter_test()
 {
-    uint64_t *ps = getPrimes(RAND_SEED), i = 5;
+    uint64_t *ps, i = 5;
 	while (i--) 
 	{
+		ps = getPrimes(RAND_SEED + i);
 		printf("%lu,%lu\n",ps[0],ps[1]);
-		ps = getPrimes(ps[0]);
+		free(ps);
 	}
 	return 0;
 }
@@ -80,6 +83,11 @@ uint64_t getter_test()
 uint64_t gcd(uint64_t a, uint64_t b)
 {
 	return ((!a) ? b : gcd(b%a,a));
+}
+
+uint64_t lcm(uint64_t a, uint64_t b)
+{
+	return (a*b)/gcd(a,b);
 }
 
 // write tests along the way, this could be main
@@ -112,123 +120,164 @@ uint64_t coprime_test()
     return 0;
 }
 
-uint64_t main()
+// get d s.t. d * e = 1 mod lamb (d is the PRIVATE KEY EXPONENT)
+// I essentially have no idea how to test this because I don't know math
+uint64_t priv_key_exp(uint64_t e, uint64_t l)
+{	
+	int i = l + 1;
+	for ( ; i % e ; i += l ) { }
+	return i;
+}
+
+// Just return the private key, we'll slice it up if we need to
+// memory leak here
+uint64_t *make_key(uint64_t seed)
 {
+	srand(seed);
+	uint64_t p = getPrime(), q = getPrime(), n, l, e, d, *ret = (uint64_t *)malloc(3 * sizeof(uint64_t));
+	ret[0] = p * q, l = lcm(p-1,q-1);
+	ret[1] = coprime(l);
+	ret[2] = priv_key_exp(ret[1], l);
+	return ret;
+}
+
+
+// fixes memory leaks
+uint64_t priv_test() //main()
+{
+	uint64_t *ret = make_key(RAND_SEED);
+	printf("priv=[%lu,%lu,%lu]\n",ret[0],ret[1],ret[2]);	
+	free(ret);
 	return 0;
+}
+
+// I can't be bothered to keep track of array lengths
+// We'll stuff ciphertexts inside these structs
+struct lenarr_struct {
+    uint64_t len, *arr;
+};
+  
+typedef struct lenarr_struct *lenarr;
+
+// memory leak here
+lenarr new_lenarr(uint64_t l, uint64_t *a)
+{
+	lenarr la = (lenarr)malloc(sizeof(lenarr)); // watch for memory leaks
+	la->len = l;
+	la->arr = a;
+	return la;
 }
 
 // so we
 // put 4 characters (32 bits) into the low bits of a uint64_t
 // return an array of uint64_t
 // we'll hash uint64_t's independently
-// need to decide how to handle length returns
-uint64_t *string_to_int(char* s)
+
+// memory leak here
+lenarr str_to_arr(char* s)
 {
-	int l = strlen(s) , i = 0;
-	uint64_t *ret = (uint64_t *)malloc(l / 4 * sizeof(uint64_t));
-    uint32_t *arr = (uint32_t *)s;
-    while ( i++ < s / 4 )
+	int l = strlen(s), i;
+	uint64_t *ret = (uint64_t *)malloc((l / 4) + 1 * sizeof(uint64_t));
+    uint32_t *arr = s;
+    for ( i = 0 ; i <= (l / 4) ; i++ )
     {
-		ret[i] = (int64_t)arr[i];
+		ret[i] = arr[i];
     }
-    return ret;
+	//printf("s2a: l = %d, i = %d, ret[0] = %lu, arr[0] = %u, str[0] = %c\n", l, i, ret[0], arr[0], s[0]);
+    return new_lenarr(i, ret);
 }
 
-//Takes in an integer and returns a character array (string)
-char *int_to_string(uint64_t *arr)
+
+// memory leak here
+char *arr_to_str(lenarr la)
 {
-    static char word_array[10];
-    uint64_t i = 0;
-    while (integer > 0)
+	int l = la->len, i;
+	char *str = (char *)malloc(l * 4 * sizeof(char));
+    uint32_t *arr = str;
+    for ( i = 0 ; i < l ; i++)
     {
-        char letter = (char) integer % 256;
-        printf("character:  %c\n", letter);
-        word_array[i] = letter;
-        integer = integer / 256;
-        i++;
+		arr[i] = (int32_t)la->arr[i];
     }
-    
-    return word_array;
-} 
-
-//c = m^e mod n
-uint64_t encrypt(uint64_t int_message, uint64_t coP, uint64_t n)
-{
-    uint64_t cipher; 
-    double power;
-
-    //power = pow((double)int_message, (double)coP);
-    //cipher = fmod(power, (double)n);
-    return cipher;
-    
+	// eliminate leading zeros
+	for ( ; !str ; str++ ) { }
+    return str;
 }
 
-
-uint64_t decrypt(uint64_t cipher, uint64_t d, uint64_t n)
+// fixes memory leaks
+uint64_t string_conversion_test() //main()
 {
-    uint64_t message;
-    double power;
-
-    //THIS POWER IS FUCKING IT ALL UP SOMEHOW
-    //power = pow((double)cipher, (double)d);
-    printf("Power1  %lf\n", power);
-
-    //message = fmod(power, (double)n);
-    return message;
+	char buf[8] = "hi";
+	printf("buf[1] = %d\n", (int)buf[1]);
+	printf("before: \"%s\"\n", buf);
+	lenarr la = str_to_arr(buf);
+	printf("lenarr length = %lu\nlenarr arr[1] = %lu\n", la->len, la->arr[1]);
+	char *str = arr_to_str(la);
+	printf("after:  \"%s\"\n", str);
+	free(la->arr);
+	free(la);
+	free(str);
+	return 0;	
 }
 
-
-uint64_t find_d(uint64_t coP, uint64_t totient)
+// modular exponentiation by squares
+// this is why we can cannot load array elements more than half full
+// realistically probably all the key parts should be in 32's - I'll think on that.
+uint64_t exp_fast(uint64_t x, uint64_t p, uint64_t m)
 {
-    for (uint64_t i = 3; i <= totient; i++){
-        if (coP * i % totient == 1){
-            return i;
-        }
-    }
-    return 0;
+	uint64_t temp;
+	x = x % m;
+	if ( !p )
+	{
+		return 1;
+	}
+	if ( p == 1 )
+	{
+		return x;
+	}
+	temp = exp_fast((x * x) % m, p / 2, m); 
+	return (p % 2) ? (x * temp) % m : temp % m ;
 }
 
-
-//Prints the final word after all the steps
-void print_final(char* word)
+uint64_t exp_test()
 {
-    printf("###################\n");
-    printf("PRINTING FINAL WORD:\n");
-    for (uint64_t i = sizeof(word) / sizeof(char); i >= 0; i--){
-            printf("%c", word[i]);
-            word[i] = word[i];
-        }
-    printf("###################\n");
+	for (uint64_t i = 2 ; i <= 100 ; i++ )
+	{
+		printf("%lu ^ %lu = %lu \n", i, i, exp_fast(i, i, 0 - 1));
+	}
+	return 0;
 }
 
+// lets do this in place
+// looking at this theres no way its right but, alas
+void encrypt(lenarr x, uint64_t p, uint64_t m)
+{
+	for ( int i = 0 ; i < x->len ; i++ )
+	{
+		x->arr[i] = exp_fast(x->arr[i], p, m);
+	}
+	return;
+}
 
-
-uint64_t old_main(){
-    uint64_t p, q, n, totient, e, coP, k, d, c, m, greatest_common_div, cipher, int_word, decrypt_messsage;
-
-    p = getPrime();
-    q = 997;
-    n = p * q;
-    totient = (p - 1) * (q - 1);
-    coP = coprime(totient);
-    greatest_common_div = gcd(p-1, q-1);
-    d = find_d(coP, totient);
-    
-    
-    int_word = string_to_int("Paul");
-    printf("int_word == %lu\n", int_word);
-
-
-    cipher = encrypt(int_word, coP, n);
-    printf("ENCRYPTED MESSAGE  ==  %lu\n", cipher);
-    printf("\n");
-
-
-    decrypt_messsage = decrypt(cipher, d, n);
-    printf("DECRYPTED MESSAGE ==  %lu\n", decrypt_messsage);
-
-
-    char* final = int_to_string(int_word);
-    print_final(final);
-
+// forked from string test
+uint64_t main()//rsa_test() //main()
+{
+	char buf[8] = "hi";
+	uint64_t *key = make_key(RAND_SEED), n, e, d;
+	n = key[0], e = key[1], d = key[2]; // I hope
+	printf("n=%lu,e=%lu,d=%lu\n", n, e, d);
+	printf("buf[1] = %d\n", (int)buf[1]);
+	printf("before: \"%s\"\n", buf);
+	lenarr la = str_to_arr(buf);
+	printf("cipher src len = %lu\n", la->len);
+	printf("cipher src arr[0] = %lu\n", la->arr[0]);
+	encrypt(la, e, n);
+	printf("cipher enc arr[0] = %lu\n", la->arr[0]);
+	encrypt(la, d, n);
+	printf("cipher dec arr[0] = %lu\n", la->arr[0]);
+	char *str = arr_to_str(la);
+	printf("after:  \"%s\"\n", str);
+	free(la->arr);
+	free(la);
+	free(str);
+	return 0;
 }
